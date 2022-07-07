d7b09ce3b0551b6e63862eeaa3bf785d1abce773
Leonardo Faria <66142358+LeonardoFariaOliveira@users.noreply.github.com>
2022-07-07 20:35:55 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,12 +2,12 @@ classe  Principal {
   public  static  void  main ( String [] args ) {
     Fila  lf = novo  Fila ();
     Pilha  lp = new  Pilha ();
-    Contato  c1 = novo  Contato ( "Romas" , "19987" );
-    Contato  c2 = novo  Contato ( "João" , "1998" );
-    Contato  c3 = novo  Contato ( "Faria" , "19999" );
-    Contato  c4 = novo  Contato ( "Ana" , "43564" );
-    Contato  c5 = novo  Contato ( "Lucas" , "145478" );
-    Contato  c6 = novo  Contato ( "Sophia" , "1999911321" );
+    Contato  c1 = novo  Contato ( "Jirso" , "19884166987" );
+    Contato  c2 = novo  Contato ( "Philips" , "142385" );
+    Contato  c3 = novo  Contato ( "Oswald" , "210345759" );
+    Contato  c4 = novo  Contato ( "Ike" , "44166564" );
+    Contato  c5 = novo  Contato ( "Lhucas" , "99478" );
+    Contato  c6 = novo  Contato ( "Matheus" , "99238851456" );
     se . removeFila ();
     se . insereFila ( c4 );
     se . insereFila ( c2 );
