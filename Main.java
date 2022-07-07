47564f86418d3f804506188984e71fe9aa0286da
Benini12 <108905136+Benini12@users.noreply.github.com>
2022-07-07 20:34:10 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,18 @@
+classe  Principal {
+  public  static  void  main ( String [] args ) {
+    Fila  lf = novo  Fila ();
+    Pilha  lp = new  Pilha ();
+    Contato  c1 = novo  Contato ( "Romas" , "19987" );
+    Contato  c2 = novo  Contato ( "João" , "1998" );
+    Contato  c3 = novo  Contato ( "Faria" , "19999" );
+    Contato  c4 = novo  Contato ( "Ana" , "43564" );
+    Contato  c5 = novo  Contato ( "Lucas" , "145478" );
+    Contato  c6 = novo  Contato ( "Sophia" , "1999911321" );
+    se . removeFila ();
+    se . insereFila ( c4 );
+    se . insereFila ( c2 );
+    se . insereFila ( c3 );
+    se . insereFila ( c1 );
+    se . imprimeFila (); 
+    se . removeFila ();
+    se . imprimeFila ();
